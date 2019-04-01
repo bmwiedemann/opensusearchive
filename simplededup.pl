68fe15ccbba166700216bd74bf1ba8b71e49c9a7
Bernhard M. Wiedemann <bwiedemann@suse.de>
2019-04-01 13:15:40 +0200

    simplify simplededup
    we do not have to read + hash all files anymore - size stat is often enough
    we skip hardlinking files that are already fully linked

--- simplededup.pl
+++ simplededup.pl
@@ -7,31 +7,28 @@ use Digest;
 # find -type f | rev | sort | rev | $0
 
 our @dups=();
+our @prevstat;
 sub flush()
 {
-        if($#dups > 0) {
+	#print STDERR "$prevstat[3] ".scalar(@dups)."\n";
+        if($#dups > 0 && $prevstat[3] != scalar(@dups)) {
 		print STDERR "found dups: @dups\n";
 		my $ref = pop(@dups); # take from end of list to get newest mtime
 		for my $dup (@dups) {
-		    print "ln -f $ref $dup\n"
+		    print "cmp $ref $dup && ln -f $ref $dup\n"
 		}
 	}
         @dups=();
 }
 
-our $prevhash="";
 while(<>) {
     chomp;
     my $filename = $_;
-    open(my $fd, "<", $filename) or die $!;
-    my $sha = Digest->new("SHA-256");
-    $sha->addfile($fd);
-    my $hash = $sha->digest();
-    #print "hash:$hash file:$filename\n";
-    if($hash ne $prevhash) {
+    my @stat = stat($_);
+    if(defined($prevstat[7]) && $stat[7] != $prevstat[7]) {
         flush();
     }
     push(@dups, $filename);
-    $prevhash = $hash;
+    @prevstat = @stat;
 }
 flush();
