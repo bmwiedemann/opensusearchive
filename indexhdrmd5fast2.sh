2b638b638f7fec429ef3af17906c90dc337355dc
Bernhard M. Wiedemann <bernhard+gitcommit@lsmod.de>
2020-06-05 06:48:47 +0000

    indexhdrmd5fast2.sh: only index new versions

--- indexhdrmd5fast2.sh
+++ indexhdrmd5fast2.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
-export BASEDIR=/ipns/opensuse.zq1.de
-for ver in `cat /ipns/opensuse.zq1.de/history/list |grep 20200|tac` ; do
-  ./indexhdrmd5fast2 /ipns/opensuse.zq1.de/history/$ver
+ipns=`ipfs resolve /ipns/opensuse.zq1.de`
+export BASEDIR=$ipns
+cp -af $ipns/history/list .ipfs.progress
+vers=$(diff .ipfs.done .ipfs.progress |perl -ne 'if(m/^> (.*)/){print "$1\n"}'|tac)
+[ -n "$vers" ] || exit 0
+for ver in $vers ; do
+  ./indexhdrmd5fast2 $ipns/history/$ver
 done
+cp -af .ipfs.progress .ipfs.done
+echo TODO rsync -a hdrmd5cid.db{,.txt} hotel.zq1.de.:~/code/cvs/opensusearchive/
+echo press enter
+read
+rsync -a hdrmd5cid.db{,.txt} hotel.zq1.de.:~/code/cvs/opensusearchive/
